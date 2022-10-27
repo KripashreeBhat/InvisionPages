acc1174dab166b11cc65ebcbe19753c057429ba5
KripashreeBhat <kripashree.bhat@robosoftin.com>
2022-10-27 16:26:51 +0530

    today

--- Invisionpages/pages/src/app/material/material.module.ts
+++ Invisionpages/pages/src/app/material/material.module.ts
@@ -3,13 +3,16 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 const MaterialComponent =[
   MatCardModule,
   MatInputModule,
   MatButtonModule,
-  MatFormFieldModule
+  MatFormFieldModule,
+  MatIconModule
 
 ]
 
