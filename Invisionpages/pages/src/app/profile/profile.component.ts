606b89db9f0a06d15336f2306d2bc40babdc140d
kripashree1920 <kripa19bhat@gmail.com>
2022-10-28 02:03:00 +0530

    profile

--- Invisionpages/pages/src/app/profile/profile.component.ts
+++ Invisionpages/pages/src/app/profile/profile.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms'
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
+  quesionnaire! : FormGroup;
+  // additional! : FormGroup;
+  constructor(private fb : FormBuilder) { }
+ get form(){
+  return this.quesionnaire.get;
+ }
   ngOnInit(): void {
+    this.quesionnaire = this.fb.group({
+      userTitle : [''],
+      description : [''],
+      btntitle: [''],
+      btntext:[''],
+      checkbox:[''],
+      startDate:[''],
+      endDate:[''],
+      reminder:[''],
+      file:[''],
+      sheet:[''],
+      mail:['']
+    });
+
+    // let additional = this.fb.group({
+    //   startDate:[''],
+    //   endDate:[''],
+    //   reminder:[''],
+    //   file:[''],
+    //   sheet:[''],
+    //   mail:['']
+    // })
   }
 
 }
